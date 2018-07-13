f6c544264f4f9a76af81712c5bab409d905d656c
carloscastro313 <40859076+carloscastro313@users.noreply.github.com>
2018-07-13 12:20:52 -0300

    Parcial 4

--- cuatro.js
+++ cuatro.js
@@ -4,37 +4,34 @@ function mostrar()
 	var n2;
 	var r;
 
-	n1=prompt("Ingrese primer numero");
-	n2=prompt("Ingrese segundo numero");
+	n1=prompt("Primer numero");
+	n2=prompt("Segundo numero");
 
 	n1=parseInt(n1);
 	n2=parseInt(n2);
 
 	if (n1==n2) {
 		
-		alert(n1+" "+n2);
-
+		r=n1*n2;
+	
 	}else{
 		if (n1>n2) {
 			
-			r=n1/n2;
-
-			alert("la division es "+r);
-
+			r=n1-n2;
+		
 		}else{
 			if (n2>n1) {
 				
 				r=n1+n2;
-				
-				if (r<50) {
-					
-					alert("La suma es "+r+" y es menor a 50");
-				
+
+				if (r>10) {
+
+					alert("la suma es "+r+ " y supero el 10");
 				}else{
-					
 					alert(r);
 				}
 			}
 		}
 	}
+	
 }
